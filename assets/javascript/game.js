ec7020980a62020fdcd6996acbfa983ee4c1beac
Garrett <gjblythe@gmail.com>
2018-08-18 19:46:02 -0700

    working Css

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// enter variables here
+    var target = Math.floor((Math.random() * 200) + 19);   //19-200
+    var xstalOne = Math.floor((Math.random() * 12) + 1);               //1-12
+    var xstalTwo = Math.floor((Math.random() * 12) + 1);
+    var xstalThree = Math.floor((Math.random() * 12) + 1);
+    var xstalFour = Math.floor((Math.random() * 12) + 1);
+    var userScore = '';
+    var wins = 0;
+    var losses = 0;
+    var overTarget = false;
+    console.log(target, xstalFour, xstalThree, xstalTwo, xstalOne)
+
+
+
+
+//Use Calculator functions
+
+
+//use a function to reload 
