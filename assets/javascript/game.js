214a501262d544959d47cada9f383fef440f31c6
Garrett <gjblythe@gmail.com>
2018-08-20 16:03:01 -0700

    Brute Force

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,124 @@
 // enter variables here
-    var target = Math.floor((Math.random() * 200) + 19);   //19-200
-    var xstalOne = Math.floor((Math.random() * 12) + 1);               //1-12
-    var xstalTwo = Math.floor((Math.random() * 12) + 1);
-    var xstalThree = Math.floor((Math.random() * 12) + 1);
-    var xstalFour = Math.floor((Math.random() * 12) + 1);
-    var userScore = '';
+ var target = Math.floor((Math.random() * (120-19)) + 19);   //19-200
+    var xOne = Math.floor((Math.random() * 12) + 1);               //1-12
+    var xTwo = Math.floor((Math.random() * 12) + 1);
+    var xThree = Math.floor((Math.random() * 12) + 1);
+    var xFour = Math.floor((Math.random() * 12) + 1);
     var wins = 0;
     var losses = 0;
     var overTarget = false;
-    console.log(target, xstalFour, xstalThree, xstalTwo, xstalOne)
+    var userScore = 0;
+
+console.log(target, xOne, xTwo, xThree, xFour);
+
+
+$(document).ready(function() {
+    console.log('ready');
+   
+
+    function reset(){
+        target = Math.floor((Math.random() * (120-19)) + 19);
+        xOne = Math.floor((Math.random() * 12) + 1);
+        xTwo = Math.floor((Math.random() * 12) + 1);
+        xThree = Math.floor((Math.random() * 12) + 1);
+        xFour = Math.floor((Math.random() * 12) + 1);
+    };
+
+   $('#target').text(target);
+
+    $('#xOne').on('click', function()
+    {
+        userScore = userScore + xOne;
+        $('#current').text(userScore);
+        if (userScore < target) {
+            overTarget = false;
+            return;
+        } else if (userScore === target){
+            wins++;
+            $('#wins').text(wins);
+            reset();
+        } else {
+            overTarget = true;
+            losses++;
+            $('#losses').text(losses);
+            reset();
+        };
+        
+    });
+
+    $('#xTwo').on('click', function()
+    {
+        userScore = userScore + xTwo;
+        $('#current').text(userScore);
+
+        if (userScore < target) {
+            overTarget = false;
+            return;
+        } else if (userScore === target){
+            wins++;
+            $('#wins').text(wins);
+            reset();
+        } else {
+            overTarget = true;
+            losses++;
+            $('#losses').text(losses);
+            reset();
+        };
+        
+    });
+    
+    $('#xThree').on('click', function()
+    {
+        userScore = userScore + xThree;
+        $('#current').text(userScore);
+
+        if (userScore < target) {
+            overTarget = false;
+            return;
+        } else if (userScore === target){
+            wins++;
+            $('#wins').text(wins);
+            reset();
+        } else {
+            overTarget = true;
+            losses++;
+            $('#losses').text(losses);
+            reset();
+        };
+    });
+    
+    $('#xFour').on('click', function()
+    {
+        userScore = userScore + xFour;
+        $('#current').text(userScore);
+
+        if (userScore < target) {
+            overTarget = false;
+            return;
+        } else if (userScore === target){
+            wins++;
+            $('#wins').text(wins);
+            reset();
+        } else {
+            overTarget = true;
+            losses++;
+            $('#losses').text(losses);
+            reset();
+        };
+        
+    });
+
+
+
+});
+    
+
+
+    
+   
+
+   
 
 
 
 
-//Use Calculator functions
-
-
-//use a function to reload 
